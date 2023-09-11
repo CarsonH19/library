8a792492ce11a8ad5bacd97963245d0bc0ccad6e
CarsonH19 <questline.carson@gmail.com>
2023-09-11 21:34:52 +0900

    modal DOM  manipulation added

--- script.js
+++ script.js
@@ -1,63 +1,84 @@
 const myLibrary = [
-    {
-      title: 'The Shadow of Bloodmoon',
-      author: 'Elena Nightshade',
-      pages: 320,
-      read: 'No'
-    },
-    {
-      title: 'Cursed Relics: Veil of Shadows',
-      author: 'Malachi Darkweaver',
-      pages: 400,
-      read: ''
-    },
-    {
-      title: 'Whispers of the Forgotten',
-      author: 'Lilith Shadowbane',
-      pages: 280,
-      read: 'Yes'
-    }
-  ];
+  {
+    title: 'The Shadow of Bloodmoon',
+    author: 'Elena Nightshade',
+    pages: 320,
+    read: 'No'
+  },
+  {
+    title: 'Cursed Relics: Veil of Shadows',
+    author: 'Malachi Darkweaver',
+    pages: 400,
+    read: ''
+  },
+  {
+    title: 'Whispers of the Forgotten',
+    author: 'Lilith Shadowbane',
+    pages: 280,
+    read: 'Yes'
+  }
+];
 
 function Book(title, author, pages, read){
-    this.title = title
-    this.author = author
-    this.pages = pages 
-    this.read = read 
+  this.title = title
+  this.author = author
+  this.pages = pages 
+  this.read = read 
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
 }
 
+// Modal Functions (Opening & Closing)
+document.addEventListener('DOMContentLoaded', function() {
+  const openModalBtn = document.getElementById('openModalBtn');
+  const modal = document.getElementById('modal');
+  const closeModalBtn = document.getElementById('closeModal');
+
+  openModalBtn.addEventListener('click', function() {
+      modal.style.display = 'block';
+  });
+
+  closeModalBtn.addEventListener('click', function() {
+      modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', function(event) {
+      if (event.target === modal) {
+          modal.style.display = 'none';
+      }
+  });
+});
+
 function displayBooks() {
-    const container = document.getElementById('book-container');
-  
-    myLibrary.forEach(book => {
-      const bookDiv = document.createElement('div');
-      bookDiv.classList.add('book');
-  
-      const title = document.createElement('h2');
-      title.textContent = `Title: ${book.title}`;
-  
-      const author = document.createElement('p');
-      author.textContent = `Author: ${book.author}`;
-  
-      const pages = document.createElement('p');
-      pages.textContent = `Pages: ${book.pages}`;
-  
-      const read = document.createElement('p');
-      read.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
-  
-      bookDiv.appendChild(title);
-      bookDiv.appendChild(author);
-      bookDiv.appendChild(pages);
-      bookDiv.appendChild(read);
-  
-      container.appendChild(bookDiv);
-    });
-  }
+  const container = document.getElementById('book-container');
+
+  myLibrary.forEach(book => {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+
+    const title = document.createElement('h2');
+    title.textContent = `Title: ${book.title}`;
+
+    const author = document.createElement('p');
+    author.textContent = `Author: ${book.author}`;
+
+    const pages = document.createElement('p');
+    pages.textContent = `Pages: ${book.pages}`;
+
+    const read = document.createElement('p');
+    read.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+
+    bookDiv.appendChild(title);
+    bookDiv.appendChild(author);
+    bookDiv.appendChild(pages);
+    bookDiv.appendChild(read);
+
+    container.appendChild(bookDiv);
+  });
+}
   
   // Call the displayBooks function to display the books
   displayBooks();
